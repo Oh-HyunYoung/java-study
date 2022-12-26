0d9c1add786f2510a3368eb2f2b7a431cc840a0e
Oh-HyunYoung <gusdud2709@gmail.com>
2022-12-26 09:54:38 +0900

    Generic Method

--- practice04/src/main/java/prob01/Printer.java
+++ practice04/src/main/java/prob01/Printer.java
@@ -6,23 +6,31 @@ public class Printer {
 	public int getPrintln() {
 		return println;
 	}
-	public void setPrintln(int println) {
-		this.println = println;
+//	public void println(int i) {
+//		System.out.println(i);
+//	}
+//	public void println(boolean i) {
+//		System.out.println(i);
+//	}
+//	public void println(double i) {
+//		System.out.println(i);
+//	}
+//	public void println(String i) {
+//		System.out.println(i);
+//	}
+	public <T> void println(T... ts) {
+		for(T t:ts) {
+		System.out.println(t);
+		}
 	}
 	
-	public void println(int i) {
-		System.out.println(i);
+	public int sum(Integer... nums) {
+		int s= 0;
+		for(Integer n:nums) {
+			s +=n;
+		}
+		return s;
 	}
-	public void println(boolean i) {
-		System.out.println(i);
-	}
-	public void println(double i) {
-		System.out.println(i);
-	}
-	public void println(String i) {
-		System.out.println(i);
-	}
+	
 
 }
-//
-//
--- practice04/src/main/java/prob01/PrinterTest.java
+++ practice04/src/main/java/prob01/PrinterTest.java
@@ -9,5 +9,13 @@ public class PrinterTest {
 		printer.println( true );
 		printer.println( 5.7 );
 		printer.println( "홍길동" );
+	
+		printer.println(10, true);
+		printer.println(10,true,"둘리"); // 아무 타입이나 대입 가능
+		
+		System.out.println(printer.sum(1));
+		System.out.println(printer.sum(1,2));
+		System.out.println(printer.sum(1,2,3,4,5));
+		System.out.println(printer.sum(1,2,3,4,5,6,7,8,9,10)); // 가변 파라미터
 	}
 }
